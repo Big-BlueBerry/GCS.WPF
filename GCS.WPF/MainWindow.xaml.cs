46eafd7e489bf152332f3075c772a3a0e708672c
phillyai <sonagi.philly@gmail.com>
2017-09-07 12:01:48 +0900

    Remove toggle button border, Add State

--- GCS.WPF/MainWindow.xaml.cs
+++ GCS.WPF/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -15,14 +16,33 @@ using System.Windows.Shapes;
 
 namespace GCS.WPF
 {
-    /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private State _currentState;
         public MainWindow()
         {
             InitializeComponent();
+            _currentState = State.NOTDRAWING;
+        }
+
+        private void Shape_Toggle(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as ToggleButton;
+            if (btn.IsChecked ?? false)
+            {
+                circleBtn.IsChecked = lineBtn.IsChecked = dotBtn.IsChecked = false;
+                btn.IsChecked = true;
+                switch (btn.Content.ToString())
+                {
+                    case "circle":
+                        _currentState = State.DRAWING_CIRCLE; break;
+                    case "line":
+                        _currentState = State.DRAWING_LINE; break;
+                    case "dot":
+                        _currentState = State.DRAWING_DOT; break;
+                }
+            }
+            else _currentState = State.NOTDRAWING;
         }
     }
 }
---
+++ GCS.WPF/State.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCS.WPF
+{
+    [Flags]
+    public enum State
+    {
+        NOTDRAWING = 0,
+        DRAWING = 1,
+    }
+}
