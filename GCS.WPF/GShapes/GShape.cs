fafb270330e33e23cba493b311091d431ec492af
phillyai <sonagi.philly@gmail.com>
2017-09-14 12:55:48 +0900

    기초적인 GShapeRule 부모관계 구축

--- GCS.WPF/GShapes/GShape.cs
+++ GCS.WPF/GShapes/GShape.cs
@@ -1,9 +1,50 @@
-﻿using System.Windows.Shapes;
+﻿using System.Collections.Generic;
+using System.Windows.Shapes;
 
 namespace GCS.WPF.GShapes
 {
     public abstract class GShape
     {
+        public abstract class GShapeRule
+        {
+            public GShape gShape;
+            public GShapeRule(GShape shape)
+            {
+                gShape = shape;
+            }
+
+            protected bool _isMoving = false;
+            public void Move()
+            {
+                if (_isMoving) return;
+                _isMoving = true;
+
+                HandleMove();
+
+                Fix();
+                MoveChilds();
+                _isMoving = false;
+            }
+
+            public void MoveChilds()
+            {
+                foreach (var c in gShape.Childs)
+                    c._rule.OnParentMoved();
+            }
+
+            public void OnParentMoved()
+            {
+                if (_isMoving) return;
+                Fix();
+                MoveChilds();
+            }
+
+            public abstract void Fix();
+            public abstract void HandleMove();
+        }
+
         public Shape Control { get; protected set; }
+        public List<GShape> Childs;
+        protected GShapeRule _rule;
     }
 }
